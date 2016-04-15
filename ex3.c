46e2b0081542dc12aa374a2908ff8c1a9b00d202
shaun10 <shutchins8@gatech.edu>
2016-04-15 16:17:14 -0500

    Notes through exercise 4 and initital env setup

---
+++ ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	int age = 27;
+	int height = 185;
+
+	printf("I am %d years old.\n", age);
+	printf("I am %d centimeters tall.\n", height);
+	
+	return 0;
+}
---
+++ ex4.c
@@ -0,0 +1,9 @@
+int main(){
+	int age = 10;
+	int height = "hi";
+	
+	printf("I am %s years old.\n");
+	printf("I am %s inches tall.\n");
+
+	return 0;
+}
